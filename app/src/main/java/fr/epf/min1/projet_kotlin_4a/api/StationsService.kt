a2d0632e338b5f5a09ae5423915921333e15467d
Tristan Bourhis <tristan.bourhis@epfedu.fr>
2022-06-05 21:10:39 +0200

    Connection to API

---
+++ app/src/main/java/fr/epf/min1/projet_kotlin_4a/api/StationsService.kt
@@ -0,0 +1,12 @@
+package fr.epf.min1.projet_kotlin_4a.api
+
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface StationsService {
+
+    @GET("station_information.json")
+    fun getStations() : Call<JsonObject>
+
+}
