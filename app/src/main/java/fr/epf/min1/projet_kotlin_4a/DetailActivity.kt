1690088f681c28ba1123b3f44a561daa7f31c77a
Tristan Bourhis <tristan.bourhis@epfedu.fr>
2022-06-08 16:46:37 +0200

    add menu detail

--- app/src/main/java/fr/epf/min1/projet_kotlin_4a/DetailActivity.kt
+++ app/src/main/java/fr/epf/min1/projet_kotlin_4a/DetailActivity.kt
@@ -1,13 +1,20 @@
 package fr.epf.min1.projet_kotlin_4a
 
+import android.content.ClipData
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 
 class DetailActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        //verifier si la station est en favoris pour init l'item du menu
 
         val nameTextView = findViewById<TextView>(R.id.detailNameTextView)
         nameTextView.text = intent.getStringExtra("name")
@@ -23,5 +30,21 @@ class DetailActivity : AppCompatActivity() {
         val nbPlace = capacity - nbVelo
         val nbPlaceTextView = findViewById<TextView>(R.id.nbPlaceTextView)
         nbPlaceTextView.text = nbPlace.toString()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.detail, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.favorite_detail_button -> {
+                //TODO
+                //fill heart
+                //add to favorite
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
